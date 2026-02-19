518eb4b3333268437071123c446b6c1e6357d0ca
Claire Lee <jclairelee2@gmail.com>
2026-02-18 16:06:56 -0800

    Setup project after forking template

--- resolvers/resolvers.js
+++ resolvers/resolvers.js
@@ -1,3 +1,1 @@
-import MovieModel from '../models/movie.js';
-
-// Resolvers define the technique for fetching the types defined in the schema.
+import MovieModel from "../models/movie.js";
--- schemas/schema.js
+++ schemas/schema.js
@@ -1,1 +1,41 @@
-import { gql } from 'graphql-tag';
+import { gql } from "graphql-tag";
+
+const movieSchema = gql`
+  type Movie {
+    id: ID!
+    name: String!
+    director_name: String!
+    production_house: String!
+    release_date: String!
+    rating: Int!
+  }
+
+  type Query {
+    getAllMovies: [Movie]
+    getMovieById(id: ID!): Movie
+    getMoviesByDirector(director_name: String!): [Movie]
+  }
+
+  type Mutation {
+    addMovie(
+      name: String!
+      director_name: String!
+      production_house: String!
+      release_date: String!
+      rating: Int!
+    ): Movie
+
+    updateMovie(
+      id: ID!
+      name: String
+      director_name: String
+      production_house: String
+      release_date: String
+      rating: Int
+    ): Movie
+
+    deleteMovie(id: ID!): String
+  }
+`;
+
+export default movieSchema;
